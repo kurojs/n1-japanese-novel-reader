d457dc3e78575337c1273b435e570fc0076b9ad0
kurojs <kuuro.js@gmail.com>
2026-05-18 19:05:55 -0600

    fix: restore top padding to reader container

--- apps/web/src/lib/components/book-reader/book-reader.svelte
+++ apps/web/src/lib/components/book-reader/book-reader.svelte
@@ -271,7 +271,7 @@
     The reader is currently blurred due to an external application (e. g. exstatic)
   </div>
 {/if}
-<div bind:this={$containerEl$} class="{pxReader} py-4">
+<div bind:this={$containerEl$} class="{pxReader} pt-16 pb-8">
   {#if viewMode === ViewMode.Continuous}
     <BookReaderContinuous
       {htmlContent}
